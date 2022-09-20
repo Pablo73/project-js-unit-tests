ad78b2d325437f0c9811738cdab879887856f911
Pablo Dominguez <pasaxo@gmail.com>
2022-09-20 15:52:39 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -11,6 +11,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+    
+    if (typeof array[index] === 'string') {
+     return undefined;
+    }
+  }
+  return (Math.round(sum / array.length));
+};
 
 module.exports = average;
